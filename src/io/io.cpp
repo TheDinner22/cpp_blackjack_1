8fd69a9647262e3a162b84c9d10d78a480f4773b
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2023-07-09 11:05:19 -0400

    removed dumb commnet

--- src/io/io.cpp
+++ src/io/io.cpp
@@ -2,8 +2,6 @@
 #include <string>
 
 // returns a pointer which YOU are responsible for
-// you need to delete it when you are done
-// with it
 std::string input(const char* prompt){
     std::cout << prompt << std::endl;
 
