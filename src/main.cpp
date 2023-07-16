38f0c6fc27fce1eac99e730b44f61fc605d83214
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2023-07-16 12:03:59 -0400

    started main game loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <array>
 #include <iostream>
 #include <string>
+#include <vector>
 #include "io/io.h"
 #include "player/player.h"
 #include "deck/deck.h"
@@ -40,11 +41,47 @@ void test_player_creation (){
 }
 
 int main(){
-    // test_input();
+    const int MAX_PLAYERS = 5;
+    // welcome message and how many players
+    std::cout << "Welcome to my garbage blackjack game!" << std::endl;
+    std::string user_input = input("How many players");
+    // https://stackoverflow.com/questions/7663709/how-can-i-convert-a-stdstring-to-int
+    int number_of_players = atoi( user_input.c_str() );
 
-    // test_card_name();
+    if (number_of_players < 1 || number_of_players > MAX_PLAYERS) {
+        std::cout << "invalid input!";
+        return 1;
+    }
 
-    // test_player_creation();
+    // make players
+    std::vector<Player> players;
+    for (int i = 0; i < number_of_players; i++) {
+        std::cout << "what is player " << i+1 << "s name?" << std::endl;
+        std::string name = input("");
+        players.push_back(Player(name));
+    }
 
-    test_deck();
+    // make deck
+    Deck deck = Deck();
+
+    while (true) {
+        // deal everyone their cards, starting with the dealer
+        const Card& dealers_shown_card = deck.draw_card();
+        const Card& dealers_hidden_card = deck.draw_card();
+        std::cout << "the dealer is showing a " << dealers_shown_card.name() << std::endl;
+
+        for (int i = 0; i < number_of_players; i++) {
+            const Card& card1 = deck.draw_card();
+            const Card& card2 = deck.draw_card();
+            players[i].add_card(card1);
+            players[i].add_card(card2);
+            players[i].print_hand();
+        }
+
+        // end of turn reset deck and player hands
+        deck.reset();
+        for (int i = 0; i < number_of_players; i++) {
+            players[i].reset_hand();
+        }
+    }
 }
